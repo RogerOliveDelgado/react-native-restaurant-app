e43368e49293b5918f69e47a280e7d3685b9cb09
RogerOliveDelgado <rogerolivedelgado@gmail.com>
2022-08-31 16:45:28 +0200

    Delete cleanArchitecture for wishlist

--- src/interfaces/WishlistEntity.ts
+++
@@ -1,5 +0,0 @@
-import {Restaurant} from './RestaurantEntity'
-
-export interface WishList {
-    Restaurants: Restaurant[]
-}
--- src/useCases/getWishlistUseCase.ts
+++
@@ -1,1 +0,0 @@
-import type { WishlistStore } from "../domain/WishlistStore";
